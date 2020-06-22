1e1de9ad8dbe5ecb8a30b2431a43cdcdd93aee8a
Naib Residov <residovnaib@gmail.com>
2020-06-22 18:46:49 +0300

    Addded_repostory_Dll

---
+++ ConstructionSite.Repository/Abstract/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.Repository.Abstract
+{
+   public interface IGenericRepository<T> where T:class
+    {
+    }
+}
---
+++ ConstructionSite.Repository/Concreate/UnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.Repository.Concreate
+{
+   public class UnitOfWork
+    {
+    }
+}
