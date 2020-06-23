44e6abbd3859bbbe0eb367ac1c20be3652b46e04
Naib Residov <residovnaib@gmail.com>
2020-06-23 19:08:10 +0300

    Added_IGenericRepository_GetAll

--- ConstructionSite.Repository/Abstract/IGenericRepository.cs
+++ ConstructionSite.Repository/Abstract/IGenericRepository.cs
@@ -6,5 +6,12 @@ namespace ConstructionSite.Repository.Abstract
 {
    public interface IGenericRepository<T> where T:class
     {
+        ICollection<T> GetAll();
+
+        //Task<ICollection<T>> GetAllAsync();
+
+        //T GetById(int id);
+
+        //Task<T> GetByIdAsync(int id);
     }
 }
