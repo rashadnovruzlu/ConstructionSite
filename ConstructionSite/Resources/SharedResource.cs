4a9eed5d3290e5ec2a3a33c78f051186cfdfdab6
Naib Residov <residovnaib@gmail.com>
2020-06-24 14:14:50 +0300

    Added_Resource_File

--- ConstructionSite/Resources/SharedResource.cs
+++ ConstructionSite/Resources/SharedResource.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ConstructionSite.Resources
+﻿namespace Mursheed.WebCoreMVC_3_1.Resources
 {
     public class SharedResource
     {
