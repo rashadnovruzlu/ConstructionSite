aa219028ea3196c01469418619ddf6a5327957f4
Naib Residov <residovnaib@gmail.com>
2020-06-24 23:17:45 +0300

    ApplicationUser

---
+++ ConstructionSite.Entity/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.Entity.Identity
+{
+   public class ApplicationUser:IdentityUser
+    {
+    }
+}
