4f35ba0081c1331179b39ec407010973206d8bcc
Naib Residov <residovnaib@gmail.com>
2020-06-24 11:24:15 +0300

    Added_Exteions_Folder_ImageExtension

---
+++ ConstructionSite.Extensions/Images/ImageExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionSite.Extensions.Images
+{
+  public static  class ImageExtensions
+    {
+        public static bool IsImage(this IFormFile file)
+        {
+            return file.ContentType == "image/jpeg" ||
+                   file.ContentType == "image/jpg" ||
+                   file.ContentType == "image/png" ||
+                   file.ContentType == "image/x-png" ||
+                   file.ContentType == "image/gif";
+        }
+
+        public static async Task<string> SaveAsync(this IFormFile file, string root, string subFolder)
+        {
+            string fileName = Path.Combine(subFolder, Guid.NewGuid().ToString() + Path.GetFileName(file.FileName));
+
+            string path = Path.Combine(root, "images", fileName);
+
+            await using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return fileName;
+        }
+    }
+}
