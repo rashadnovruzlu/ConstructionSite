a9bea762f7d2e16bb0b6b708df46b0a374c5b90f
Naib Residov <residovnaib@gmail.com>
2020-06-24 17:57:22 +0300

    Delete_ResoucesKeyFolder

--- ConstructionSite.Helpers/Constants/ResourceKeys.cs
+++ ConstructionSite.Helpers/Constants/ResourceKeys.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ConstructionSite.Helpers.ControllerResources
+namespace ConstructionSite.Helpers.Constants
 {
     public static class ResourceKeys
     {
