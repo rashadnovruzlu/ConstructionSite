f551b639864ca4e66b0a9a1ddecfd31a1762f1a6
Naib Residov <residovnaib@gmail.com>
2020-06-25 22:11:43 +0300

    LoginModel_but_not_Finished

---
+++ ConstructionSite/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionSite.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Logion(string returnUrl)
+        {
+            ViewBag.returnUrl= returnUrl;
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public IActionResult Logion()
+        {
+            return View();
+        }
+    }
+}
