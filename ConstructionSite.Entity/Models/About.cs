0f942a4bb3e75560fe25c5d7ff155202b4097d95
Nurana Panahli <penahlinurana@gmail.com>
2020-06-26 11:56:07 +0300

    Db ucun About class-inin uzerinde deyisiklikleri etdim

--- ConstructionSite.Entity/Models/About.cs
+++ ConstructionSite.Entity/Models/About.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ConstructionSite.Entity.Models
 {
 	public class About
 	{
+		[Required]
 		public int Id { get; set; }
-		public string Title { get; set; }
-		public string Content { get; set; }
+		[Required]
+		[MaxLength(250)]
+		public string TittleAz { get; set; }
+		[MaxLength(250)]
+		public string TittleRu { get; set; }
+		[MaxLength(250)]
+		public string TittleEn { get; set; }
+		[Required]
+		public string ContentAz { get; set; }
+		public string ContentRu { get; set; }
+		public string ContentEn { get; set; }
 	}
 }
