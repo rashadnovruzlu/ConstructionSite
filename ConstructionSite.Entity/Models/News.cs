89ff16da98a3594ea95e89e6bcc605d9db2cec5f
Nurana Panahli <penahlinurana@gmail.com>
2020-06-26 14:20:24 +0300

    News class-daki uygun property-leri qeyd etdim

---
+++ ConstructionSite.Entity/Models/News.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ConstructionSite.Entity.Models
+{
+    class News
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string TittleAz { get; set; }
+        [MaxLength(150)]
+        public string TittleEn { get; set; }
+        [MaxLength(150)]
+        public string TittleRu { get; set; }
+        [Required]
+        public string ContentAz { get; set; }
+        public string ContentEn { get; set; }
+        public string ContentRu { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime CreateDate { get; set; }
+
+    }
+}
