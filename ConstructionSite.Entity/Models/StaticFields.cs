fa15b1f3782926990a4db5bd2c0b3d493ea41940
Nurana Panahli <penahlinurana@gmail.com>
2020-06-26 17:02:47 +0300

    StaticFields class-ni bitirdim

---
+++ ConstructionSite.Entity/Models/StaticFields.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ConstructionSite.Entity.Models
+{
+    class StaticFields
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string Key { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string Value { get; set; }
+    }
+}
