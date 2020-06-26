122cf8897b24a2394ad15353aee82358cd8719e6
Nurana Panahli <penahlinurana@gmail.com>
2020-06-26 16:56:37 +0300

    Service class-daki property-leri qeyd etdim

---
+++ ConstructionSite.Entity/Models/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ConstructionSite.Entity.Models
+{
+    class Project
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(75)]
+        public string NameAz { get; set; }
+        [MaxLength(75)]
+        public string NameEn { get; set; }
+        [MaxLength(75)]
+        public string NameRu { get; set; }
+        [Required]
+        public string ContentAz { get; set; }
+        public string ContentRu { get; set; }
+        public string ContentEn { get; set; }
+
+        public Portfolio Portfolio { get; set; }
+        [Required]
+        public int PortfolioId { get; set; }
+    }
+}
--- ConstructionSite.Entity/Models/Service.cs
+++ ConstructionSite.Entity/Models/Service.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ConstructionSite.Entity.Models
 {
 	public class Service
 	{
+		[Required]
 		public int Id { get; set; }
+		[Required]
+		[MaxLength(75)]
 		public string NameAz { get; set; }
-
+		[MaxLength(75)]
+		public string NameEn { get; set; }
+		[MaxLength(75)]
+		public string NameRu { get; set; }
+		[Required]
+		public string TittleAz { get; set; }
+		public string TittleEn { get; set; }
+		public string TittleRu { get; set; }
 		
+		public Image Image { get; set; }
+		public int ImageId { get; set; }
 	}
 }
