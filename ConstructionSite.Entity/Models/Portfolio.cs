95f4684c2895ef08fd20c67c197dedaf03795d95
Nurana Panahli <penahlinurana@gmail.com>
2020-06-26 16:22:09 +0300

    Portfolio class-ni yaratdim

---
+++ ConstructionSite.Entity/Models/Portfolio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ConstructionSite.Entity.Models
+{
+    class Portfolio
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(75)]
+        public string NameAz { get; set; }
+        [MaxLength(75)]
+        public string NameEn { get; set; }
+        [MaxLength(75)]
+        public string NameRu { get; set; }
+
+    }
+}
