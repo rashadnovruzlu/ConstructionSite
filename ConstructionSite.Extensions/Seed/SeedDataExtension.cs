290e02b814755f123e3f21c6ef6169e7d554a7a3
Naib Residov <residovnaib@gmail.com>
2020-06-27 22:00:26 +0300

    SeedDataExtension

--- ConstructionSite.Extensions/Seed/SeedDataExtension.cs
+++ ConstructionSite.Extensions/Seed/SeedDataExtension.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using ConstructionSite.Entity.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ConstructionSite.Extensions.Seed
 {
-   public class SeedDataExtension
+   public static class SeedDataExtension
     {
+        public static void Seeding(IApplicationBuilder app)
+        {
+            var context=app.ApplicationServices.GetRequiredService<ConstructionDbContext>();
+            context.Database.Migrate();
+        }
     }
 }
