9387dedd6ed47ba5436515db8222a0f376750fc5
Naib Residov <residovnaib@gmail.com>
2020-06-29 09:30:15 +0300

    Message

---
+++ ConstructionSite.Helpers/Interfaces/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿
+using ConstructionSite.Helpers.Content;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionSite.Helpers.Interfaces
+{
+   public interface IEmailSender
+    {
+        Task SendEmailAsync(Message message);
+    }
+}
