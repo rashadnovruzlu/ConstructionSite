5b321e253047889978b03129267d2cda57c5d689
Nurana Panahli <penahlinurana@gmail.com>
2020-06-30 23:34:41 +0300

    ImageDTO-nu qeyd etdim

---
+++ ConstructionSite.DTO/ModelsDTO/ImageDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.DTO.ModelsDTO
+{
+    class ImageDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Path { get; set; }
+    }
+}
