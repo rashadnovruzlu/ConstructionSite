567a5e00be4cd887d970bcea73a339fd8b13a8d7
Nurana Panahli <penahlinurana@gmail.com>
2020-06-30 15:21:01 +0300

    Blog/Index.cshtml-i yerlesdirdim

---
+++ ConstructionSite/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionSite.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Inner()
+        {
+            return View();
+        }
+    }
+}
