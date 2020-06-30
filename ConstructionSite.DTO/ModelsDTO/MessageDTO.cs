d23df8891ace3f2a87dda29f2c4bff45a52dccd7
Nurana Panahli <penahlinurana@gmail.com>
2020-06-30 23:37:57 +0300

    MessageDTO-nu qeyd etdim

---
+++ ConstructionSite.DTO/ModelsDTO/MessageDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.DTO.ModelsDTO
+{
+    class MessageDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsAnswerd { get; set; }
+    }
+}
