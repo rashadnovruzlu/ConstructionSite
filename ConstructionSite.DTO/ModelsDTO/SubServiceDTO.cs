de19b5f61dca92f71d0a24b324b809d407324029
Nurana Panahli <penahlinurana@gmail.com>
2020-07-01 00:52:24 +0300

    SubServiceDTO-nu elave etdim

--- ConstructionSite.DTO/ModelsDTO/SubServiceDTO.cs
+++ ConstructionSite.DTO/ModelsDTO/SubServiceDTO.cs
@@ -4,14 +4,15 @@ using System.Text;
 
 namespace ConstructionSite.DTO.ModelsDTO
 {
-    class ServiceDTO
+    class SubServiceDTO
     {
         public int Id { get; set; }
         public string NameAz { get; set; }
         public string NameEn { get; set; }
         public string NameRu { get; set; }
-        public string TittleAz { get; set; }
-        public string TittleEn { get; set; }
-        public string TittleRu { get; set; }
+        public string ContentAz { get; set; }
+        public string ContentRu { get; set; }
+        public string ContentEn { get; set; }
+        public int ServiceId { get; set; }
     }
 }
