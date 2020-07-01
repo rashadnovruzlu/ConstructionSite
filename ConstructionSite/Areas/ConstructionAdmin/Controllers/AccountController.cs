1294aabac09beb9174662707f63d3970d7630dc9
Nurana Panahli <penahlinurana@gmail.com>
2020-07-01 22:18:26 +0300

    Controller-de Create-i qeyd etdim [HttpGet], [HttpPost]-lari elave etdim

--- ConstructionSite/Areas/ConstructionAdmin/Controllers/AccountController.cs
+++ ConstructionSite/Areas/ConstructionAdmin/Controllers/AccountController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ConstructionSite.DTO;
 using ConstructionSite.Areas.ConstructionAdmin.Models.DTO;
+using Microsoft.EntityFrameworkCore;
+using ConstructionSite.Areas.ConstructionAdmin.Models.ViewModels;
 
 namespace ConstructionSite.Areas.Admin.Controllers
 {
@@ -18,12 +20,24 @@ namespace ConstructionSite.Areas.Admin.Controllers
     {
         private readonly UserManager<ApplicationUser> userManager;
         private readonly SignInManager<ApplicationUser> SignInManager;
-        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> SignInManager)
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> SignInManager, RoleManager<IdentityRole> roleManager)
         {
             this.userManager=userManager;
             this.SignInManager=SignInManager;
+            this._roleManager = roleManager;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             IEnumerable<UserDTO> users = userManager.Users.Select(m => new UserDTO
@@ -33,6 +47,72 @@ namespace ConstructionSite.Areas.Admin.Controllers
                 Email=m.Email
             });
             return View(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(new UserViewModel
+            {
+                Roles = roles
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(UserViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ApplicationUser user = new ApplicationUser
+                    {
+                        UserName = viewModel.Username,
+                        Name = viewModel.Name,
+                        Email = viewModel.Email
+                    };
+
+                    IdentityResult result = await userManager.CreateAsync(user, viewModel.Password);
+
+                    if(result.Succeeded)
+                    {
+                        ApplicationUser appUser = await userManager.FindByNameAsync(user.UserName);
+                        IdentityRole role = await _roleManager.FindByIdAsync(viewModel.RoleId);
+                        IdentityResult identityResult = await userManager.AddToRoleAsync(appUser, role.Name);
+
+                        if (identityResult.Succeeded)
+                            return RedirectToAction(nameof(Index));
+                        else
+                        {
+                            AddErrors(result);
+
+                            if (ModelState.ErrorCount != 0)
+                            {
+                                viewModel.Roles = await _roleManager.Roles.ToListAsync();
+                                return View(viewModel);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        AddErrors(result);
+
+                        if (ModelState.ErrorCount != 0)
+                        {
+                            viewModel.Roles = await _roleManager.Roles.ToListAsync();
+                            return View(viewModel);
+                        }
+                    }
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Some error occured. Please try again.");
+                }
+            }
+            viewModel.Roles = await _roleManager.Roles.ToListAsync();
+            return View(viewModel);
         }
 
         [HttpGet]
