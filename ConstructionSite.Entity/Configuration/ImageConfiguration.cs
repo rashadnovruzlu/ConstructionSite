6bdf367fd8cf5d9e50213d95e61ece05c22e7800
Naib Residov <residovnaib@gmail.com>
2020-07-03 15:31:28 +0300

    Configuration_Modfiay

---
+++ ConstructionSite.Entity/Configuration/ImageConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using ConstructionSite.Entity.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ConstructionSite.Entity.Configuration
+{
+    public class ImageConfiguration : IEntityTypeConfiguration<Image>
+    {
+        public void Configure(EntityTypeBuilder<Image> builder)
+        {
+            builder.HasKey(x=>x.Id);
+            builder.Property(x=>x.Title)
+                .IsRequired()
+                .HasMaxLength(150);
+            builder.Property(x=>x.Path)
+                .IsRequired()
+                .HasMaxLength(255);
+        }
+    }
+   
+
+
+
+
+
+
+
+}
--- ConstructionSite.Entity/Configuration/SubServiceConfiguration.cs
+++ ConstructionSite.Entity/Configuration/SubServiceConfiguration.cs
@@ -4,28 +4,27 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ConstructionSite.Entity.Configuration
 {
-    public class ServiceConfiguration : IEntityTypeConfiguration<Service>
+    public class SubServiceConfiguration : IEntityTypeConfiguration<SubService>
     {
-        public void Configure(EntityTypeBuilder<Service> builder)
-        {    
-           
+        public void Configure(EntityTypeBuilder<SubService> builder)
+        {
             builder.HasKey(x=>x.Id);
             builder.Property(x=>x.NameAz)
                 .IsRequired()
                 .HasMaxLength(75);
-            
+
             builder.Property(x => x.NameRu)
-             .HasMaxLength(75);
+               
+               .HasMaxLength(75);
 
             builder.Property(x => x.NameEn)
-              .HasMaxLength(75);
-
-            builder.Property(x=>x.TittleAz)
+               
+               .HasMaxLength(75);
+            builder.Property(x=>x.ContentAz)
                 .IsRequired();
 
-          
-               
-                                  
+            builder.HasOne(ss => ss.Service)
+                                .WithMany(s => s.SubServices);
         }
     }
    
