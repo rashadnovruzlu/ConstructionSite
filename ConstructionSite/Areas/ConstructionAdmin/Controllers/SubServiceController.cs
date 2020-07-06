282c558c26c585c73fae0a28718b84590e99870d
Naib Residov <residovnaib@gmail.com>
2020-07-07 00:58:35 +0300

    SubServiceController

--- ConstructionSite/Areas/ConstructionAdmin/Controllers/SubServiceController.cs
+++ ConstructionSite/Areas/ConstructionAdmin/Controllers/SubServiceController.cs
@@ -1,31 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-using ConstructionSite.Entity.Models;
+﻿using ConstructionSite.Entity.Models;
 using ConstructionSite.Extensions.Images;
 using ConstructionSite.Repository.Abstract;
-using ConstructionSite.Repository.Concreate;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace ConstructionSite.Areas.ConstructionAdmin.Controllers
 {
     [Area(nameof(ConstructionAdmin))]
     public class SubServiceController : Controller
     {
-        private readonly IUnitOfWork          _unitOfWork;
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IWebHostEnvironment _env;
+
         public SubServiceController(IUnitOfWork unitOfWork,
                                     IWebHostEnvironment env)
         {
             _unitOfWork = unitOfWork;
             _env = env;
-
         }
+
         public IActionResult Index()
         {
             if (!ModelState.IsValid)
@@ -36,10 +33,10 @@ namespace ConstructionSite.Areas.ConstructionAdmin.Controllers
                 {
                     message = "BadRequest"
                 });
-
             }
             return View();
         }
+
         public IActionResult Add()
         {
             if (!ModelState.IsValid)
@@ -50,11 +47,11 @@ namespace ConstructionSite.Areas.ConstructionAdmin.Controllers
                 {
                     message = "BadRequest"
                 });
-
             }
             ViewBag.data = _unitOfWork.ServiceRepository.GetAll().ToList();
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Add(SubService subService, IFormFile file)
         {
@@ -68,47 +65,36 @@ namespace ConstructionSite.Areas.ConstructionAdmin.Controllers
                 {
                     message = "BadRequest"
                 });
-
             }
-                if (file is null)
+            if (file is null)
             {
-                Response.StatusCode=(int)HttpStatusCode.NotFound;
+                Response.StatusCode = (int)HttpStatusCode.NotFound;
                 return Json(new
                 {
                     message = "file not found BadRequest"
                 });
             }
-           
+
             sub.ImageId = await file.SaveImage(_env, "subserver", image, _unitOfWork);
-            if (sub.ImageId<0)
+            if (sub.ImageId < 0)
             {
                 return Json(new
                 {
                     message = "file not save"
                 });
             }
-             var SubServiceResult = await _unitOfWork.SubServiceRepository.AddAsync(subService);
+            var SubServiceResult = await _unitOfWork.SubServiceRepository.AddAsync(subService);
             if (SubServiceResult.IsDone)
             {
-                sub.SubServiceId=subService.Id;
+                sub.SubServiceId = subService.Id;
             }
-            var SubServiceImageResult=  await _unitOfWork.SubServiceImageRepository.AddAsync(sub);
+            var SubServiceImageResult = await _unitOfWork.SubServiceImageRepository.AddAsync(sub);
             if (!SubServiceImageResult.IsDone)
             {
-
-
                 return RedirectToAction("Index");
-
-
             }
-           ViewBag.data = _unitOfWork.ServiceRepository.GetAll().ToList();
-           return View();
-
+            ViewBag.data = _unitOfWork.ServiceRepository.GetAll().ToList();
+            return View();
         }
-
-
-
     }
-    }
-
-
+}
