c58dc8ee65e96b62d9214eb4c8df79a23e7744c2
Naib Residov <residovnaib@gmail.com>
2020-07-07 00:05:54 +0300

    ProjectController

--- ConstructionSite/Areas/ConstructionAdmin/Controllers/ProjectController.cs
+++ ConstructionSite/Areas/ConstructionAdmin/Controllers/ProjectController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using ConstructionSite.DTO.AdminViewModels;
 using ConstructionSite.Entity.Models;
@@ -14,7 +15,7 @@ namespace ConstructionSite.Areas.ConstructionAdmin.Controllers
 {
     public class ProjectController : Controller
     {
-        private readonly IUnitOfWork _unitOfWork;
+        private readonly IUnitOfWork         _unitOfWork;
         private readonly IWebHostEnvironment _env;
 
         public ProjectController(IUnitOfWork unitOfWork, IWebHostEnvironment env)
@@ -35,36 +36,63 @@ namespace ConstructionSite.Areas.ConstructionAdmin.Controllers
         [HttpPost]
         public async Task<IActionResult> Add(ProjectAddModel project,IFormFile file)
         {
-
-            if (ModelState.IsValid)
+            Image img = new Image();
+            int IMAGEID=0;
+            if (!ModelState.IsValid)
             {
-                Image img = new Image();
-                int id = await file.SaveImage(_env, "project", img, _unitOfWork);
-                var result = new Project
-                {
-                    NameAz = project.NameAz,
-                    NameRu = project.NameRu,
-                    NameEn = project.NameEn,
-                    ContentAz = project.ContentAz,
-                    ContentRu = project.ContentRu,
-                    ContentEn = project.ContentEn,
-                    PortfolioId = project.PortfolioId,
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
 
-                };
-              await  _unitOfWork.projectRepository.AddAsync(result);
+                return Json(new
+                {
+                    message = "BadRequest"
+                });
+
+            }
+            if (file is null)
+            {
+                return Json(new
+                {
+                    message = "file not exists"
+                });
+            }
+            IMAGEID = await file.SaveImage(_env, "project", img, _unitOfWork);
+            if (IMAGEID<0)
+            {
+                return Json(new
+                {
+                    message = "file not save"
+                });
+            }
+            var result = new Project
+            {
+                NameAz = project.NameAz,
+                NameRu = project.NameRu,
+                NameEn = project.NameEn,
+                ContentAz = project.ContentAz,
+                ContentRu = project.ContentRu,
+                ContentEn = project.ContentEn,
+                PortfolioId = project.PortfolioId,
+
+            };
+            var projectResult=  await _unitOfWork.projectRepository.AddAsync(result);
+            if (projectResult.IsDone)
+            {
                 ProjectImage image = new ProjectImage
                 {
-                    ImageId = id,
-                    ProjectId=result.Id
+                    ImageId = IMAGEID,
+                    ProjectId = result.Id
                 };
-                if (await _unitOfWork.projectImageRepository.AddAsync(image)>0)
+                var imageResult= await _unitOfWork.projectImageRepository.AddAsync(image);
+                if (imageResult.IsDone)
+                {
                     return RedirectToAction("Index");
-                    _unitOfWork.Dispose();
-                
-               
+                }
+                else
+                {
+                    ModelState.AddModelError("","this is error");
+                }
             }
-
-
+            _unitOfWork.Dispose();
             return View();
         }
     }
