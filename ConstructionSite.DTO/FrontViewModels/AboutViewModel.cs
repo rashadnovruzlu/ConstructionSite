7265a889859c0901d8e835a898a534f5e5e71ec4
Naib Residov <residovnaib@gmail.com>
2020-07-06 16:36:58 +0300

    Modifay_AboutViewComponent

---
+++ ConstructionSite.DTO/FrontViewModels/AboutViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.DTO.FrontViewModels
+{
+   public class AboutViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+       
+        public string Content { get; set; }
+        
+        public string image { get; set; }
+    }
+}
