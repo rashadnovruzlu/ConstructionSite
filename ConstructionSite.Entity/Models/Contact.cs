23fcd3f63cca3555d589f5c11f60d937eca0b5e8
Naib Residov <residovnaib@gmail.com>
2020-07-08 00:58:02 +0300

    Added_Data

--- ConstructionSite.Entity/Models/Contact.cs
+++ ConstructionSite.Entity/Models/Contact.cs
@@ -10,12 +10,7 @@ namespace ConstructionSite.Entity.Models
 	{
 	
 		public int Id { get; set; }
-		public string TittleAz { get; set; }
-		public string TittleEn { get; set; }
-		public string TittleRu { get; set; }
-		public string ContentAz { get; set; }
-		public string ContentEn { get; set; }
-		public string ContentRu { get; set; }
+		
 		public string Address { get; set; }
 		//[Required]
 		//[MaxLength(150)]
