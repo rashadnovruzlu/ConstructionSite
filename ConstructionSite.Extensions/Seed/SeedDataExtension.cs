f21bbc78a56a34fea06c6eff5f875975a3318d5f
Nurana Panahli <penahlinurana@gmail.com>
2020-07-08 14:16:43 +0300

    Service ve Contact uchun SeedDataExtension-u qeyd etdim

--- ConstructionSite.Extensions/Seed/SeedDataExtension.cs
+++ ConstructionSite.Extensions/Seed/SeedDataExtension.cs
@@ -25,13 +25,44 @@ namespace ConstructionSite.Extensions.Seed
                    ContentEn= "Integer rhoncus hendrerit sem egestas porttitor. Integer et mi sed dolor eleifend pretium quis ut velit. Nam sit amet arcu feugiat, consequat orci at, ultrices magna. Aliquam vestibulum, eros vel venenatis vulputate, erat augue suscipit mi, nec rhoncus velit ipsum sed lorem. Nulla commodo leo eget justo blandit, non sagittis lectus dignissim"
                 });
             }
-            //if (!await context.Images.AnyAsync())
-            //{
-            //    context.Images.Add(new Image
-            //    {
-            //        Path=
-            //    });
-            //}
+
+            if(!await context.Images.AnyAsync())
+            {
+                context.Images.Add(new Image
+                {
+
+                });
+            
+            }
+
+            if(!await context.Services.AnyAsync())
+            {
+                context.Services.Add(new Service
+                {
+                    NameAz = "Construction",
+                    NameEn = "Construction",
+                    NameRu = "Construction",
+                    TittleAz = "Sed sit amet sapien sit amet odio lobortis ullamcorper quis vel nisl. Nam blandit maximus tristique. Vivamus enim quam.",
+                    TittleEn = "Sed sit amet sapien sit amet odio lobortis ullamcorper quis vel nisl. Nam blandit maximus tristique. Vivamus enim quam.",
+                    TittleRu = "Sed sit amet sapien sit amet odio lobortis ullamcorper quis vel nisl. Nam blandit maximus tristique. Vivamus enim quam."
+                });
+            }
+
+            if(!await context.Contacts.AnyAsync())
+            {
+                context.Contacts.Add(new Contact
+                {
+                    TittleAz = "GET IN TOUCH WITH US",
+                    TittleEn = "GET IN TOUCH WITH US",
+                    TittleRu = "GET IN TOUCH WITH US",
+                    ContentAz = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet iaculis elit. Nam semper ut arcu non placerat. Praesent nibh massa varius.",
+                    ContentEn = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet iaculis elit. Nam semper ut arcu non placerat. Praesent nibh massa varius.",
+                    ContentRu = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet iaculis elit. Nam semper ut arcu non placerat. Praesent nibh massa varius.",
+                    Address = "68 Havemeyer St, Brooklyn, NY 11211 United States",
+                    Email = "contact@construction.com",
+                    PhoneNumber = "+1 718-955-2838 or +1 718-955-3290"
+                });
+            }
         }
     }
 }
