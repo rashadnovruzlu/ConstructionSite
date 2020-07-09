807ca4d3becd4bc435d362c1b083b54174e8e126
Nurana Panahli <penahlinurana@gmail.com>
2020-07-09 18:17:19 +0300

    Create templateni elave etdim

---
+++ ConstructionSite.DTO/AdminViewModels/Blog/BlogCreateModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.DTO.AdminViewModels.Blog
+{
+    public class BlogCreateModel
+    {
+		public int Id { get; set; }
+		public string TittleAz { get; set; }
+		public string TittleEn { get; set; }
+		public string TittleRu { get; set; }
+		public string ContentAz { get; set; }
+		public string ContentEn { get; set; }
+		public string ContentRu { get; set; }
+		public IFormFile Img { get; set; }
+		public int ImageId { get; set; }
+		public DateTime CreateDate { get; set; }
+	}
+}
