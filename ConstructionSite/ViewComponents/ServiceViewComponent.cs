19c3482a6351c8f23a192c2e813e5085004dea71
Naib Residov <residovnaib@gmail.com>
2020-07-09 13:15:11 +0300

    Added_Data

--- ConstructionSite/ViewComponents/ServiceViewComponent.cs
+++ ConstructionSite/ViewComponents/ServiceViewComponent.cs
@@ -8,24 +8,32 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace ConstructionSite.ViewComponents
 {
     public class ServiceViewComponent:ViewComponent
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private string _lang;
-        public ServiceViewComponent(IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor)
+        string                                    _lang;
+        private readonly IUnitOfWork              _unitOfWork;
+        private readonly IHttpContextAccessor    _httpContextAccessor;
+        public ServiceViewComponent(IUnitOfWork unitOfWork, 
+                                    IHttpContextAccessor httpContextAccessor)
         {
             _unitOfWork=unitOfWork;
-            _httpContextAccessor=httpContextAccessor;
+            _httpContextAccessor = httpContextAccessor;
             _lang=httpContextAccessor.getLang();
         }
         public  IViewComponentResult Invoke()
         {
-            var result=  _unitOfWork.ServiceRepository.GetAll()
+            if (!ModelState.IsValid)
+            {
+                _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                ModelState.AddModelError("", "BadRequest");
+            }
+                var result=  _unitOfWork.ServiceRepository.GetAll()
                 .Include(x=>x.Image)
                 .Include(x=>x.SubServices).Select(x=>new ServiceViewModel
                 {
@@ -35,7 +43,10 @@ namespace ConstructionSite.ViewComponents
                     Tittle=x.FindName(_lang)
                     
                 }).ToList();
-          
+            if (result.Count==0|result==null)
+            {
+                ModelState.AddModelError("","data not exists");
+            }
          return View(result);
                 
                 
