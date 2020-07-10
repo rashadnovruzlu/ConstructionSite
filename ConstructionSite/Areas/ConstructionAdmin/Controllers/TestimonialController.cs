a5650c6167ce7cd49f2764cbf0c92b9d841bd4d6
Naib Residov <residovnaib@gmail.com>
2020-07-10 23:55:05 +0300

    Added_data_View

--- ConstructionSite/Areas/ConstructionAdmin/Controllers/TestimonialController.cs
+++ ConstructionSite/Areas/ConstructionAdmin/Controllers/TestimonialController.cs
@@ -2,30 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ConstructionSite.Entity.Models;
+using ConstructionSite.Injections;
+using ConstructionSite.Repository.Abstract;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ConstructionSite.Areas.ConstructionAdmin.Controllers
 {
     public class TestimonialController : Controller
     {
+
+        private string   _lang;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _env;
+
+        public TestimonialController(IUnitOfWork unitOfWork,
+                                     IWebHostEnvironment env,
+                                     IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _unitOfWork = unitOfWork;
+            _env = env;
+            _lang = _httpContextAccessor.getLang();
+        }
         public IActionResult Index()
         {
             return View();
         }
         [HttpGet]
-        public IActionResult add(string str)
+        public IActionResult add()
         {
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Add()
+        public async Task<IActionResult> Add(CustomerFeedback customerFeedback)
         {
+            if (customerFeedback==null)
+            {
+              ModelState.AddModelError("","this data is null or emoty");
+            }
+           var customerFeedbackResult=await  _unitOfWork.customerFeedbackRepository.AddAsync(customerFeedback);
+
+            if (!customerFeedbackResult.IsDone)
+            {
+                _unitOfWork.Rollback();
+                ModelState.AddModelError("", "this data is not add");
+            }
+            _unitOfWork.Dispose();
             return View();
         }
         [HttpGet]
-        public IActionResult Update(int id)
+        public async Task<IActionResult> Update(int id)
         {
+          var customerFeedbackUpdateResult=await  _unitOfWork.customerFeedbackRepository.GetByIdAsync(id);
+            if (customerFeedbackUpdateResult==null)
+            {
+                ModelState.AddModelError("", "this data is null or emoty");
+            }
+           
             return View();
         }
         public IActionResult Update()
