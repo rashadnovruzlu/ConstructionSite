40bf27ad4f7f0765f79eec6cebb0936cc48ac516
Nurana Panahli <penahlinurana@gmail.com>
2020-07-10 20:00:17 +0300

    FrontViewModel folderine BlogViewModel class-ni elave etdim

---
+++ ConstructionSite.DTO/FrontViewModels/Blog/BlogViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.DTO.FrontViewModels.Blog
+{
+    public class BlogViewModel
+    {
+        public int Id { get; set; }
+        public int NewsId { get; set; }
+        public string Tittle { get; set; }
+        public string Content { get; set; }
+        public string Image { get; set; }
+        public int ImageId { get; set; }
+    }
+}
--- ConstructionSite/ViewComponents/BlogViewComponent.cs
+++ ConstructionSite/ViewComponents/BlogViewComponent.cs
@@ -1,15 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ConstructionSite.Helpers.Constants;
+using ConstructionSite.Helpers.Interfaces;
+using ConstructionSite.Injections;
+using ConstructionSite.Repository.Concreate;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace ConstructionSite.ViewComponents
 {
     public class BlogViewComponent:ViewComponent
     {
+        string _lang;
+        private readonly UnitOfWork _unitOfWork;
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly ISharedLocalizationHandle _localizationHandle;
+
+        public BlogViewComponent(UnitOfWork unitOfWork,
+                                 IHttpContextAccessor contextAccessor,
+                                 ISharedLocalizationHandle localizationHandle)
+        {
+            _unitOfWork = unitOfWork;
+            _contextAccessor = contextAccessor;
+            _localizationHandle = localizationHandle;
+            _lang = _contextAccessor.getLang();
+        }
+
         public IViewComponentResult Invoke()
-        { 
+        {
+            if (!ModelState.IsValid)
+            {
+                _contextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                ModelState.AddModelError("", _localizationHandle.GetLocalizationByKey(RESOURCEKEYS.BadRequest));
+            }
+
+            var newsImageResult= _unitOfWork.newsImageRepository.GetAll()
+                                                .Select(x=>new BlogViewModel)
+
             return View();
         }
     }
