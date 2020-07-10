083e0663787b09fdeacc3a64f7c02a2c552e587e
Naib Residov <residovnaib@gmail.com>
2020-07-11 00:07:51 +0300

    ValidateAntiForgeryToken

--- ConstructionSite/Areas/ConstructionAdmin/Controllers/TestimonialController.cs
+++ ConstructionSite/Areas/ConstructionAdmin/Controllers/TestimonialController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using ConstructionSite.DTO.AdminViewModels.Testimonial;
 using ConstructionSite.Entity.Models;
 using ConstructionSite.Injections;
 using ConstructionSite.Repository.Abstract;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +15,8 @@ using Org.BouncyCastle.Math.EC.Rfc7748;
 
 namespace ConstructionSite.Areas.ConstructionAdmin.Controllers
 {
+    [Area(nameof(ConstructionAdmin))]
+    [Authorize(Roles = "Admin")]
     public class TestimonialController : Controller
     {
 
@@ -30,19 +34,47 @@ namespace ConstructionSite.Areas.ConstructionAdmin.Controllers
             _env = env;
             _lang = _httpContextAccessor.getLang();
         }
+        [HttpGet]
         public IActionResult Index()
         {
+            if (!ModelState.IsValid)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                return Json(new
+                {
+                    message = "BadRequest"
+                });
+            }
             return View();
         }
         [HttpGet]
         public IActionResult add()
         {
+            if (!ModelState.IsValid)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                return Json(new
+                {
+                    message = "BadRequest"
+                });
+            }
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Add(CustomerFeedback customerFeedback)
         {
+            if (!ModelState.IsValid)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                return Json(new
+                {
+                    message = "BadRequest"
+                });
+            }
             if (customerFeedback==null)
             {
               ModelState.AddModelError("","this data is null or emoty");
@@ -60,7 +92,16 @@ namespace ConstructionSite.Areas.ConstructionAdmin.Controllers
         [HttpGet]
         public async Task<IActionResult> Update(int id)
         {
-          var customerFeedbackUpdateResult=await  _unitOfWork.customerFeedbackRepository.GetByIdAsync(id);
+            if (!ModelState.IsValid)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                return Json(new
+                {
+                    message = "BadRequest"
+                });
+            }
+            var customerFeedbackUpdateResult=await  _unitOfWork.customerFeedbackRepository.GetByIdAsync(id);
             if (customerFeedbackUpdateResult==null)
             {
                 ModelState.AddModelError("", "this data is null or emoty");
@@ -76,12 +117,55 @@ namespace ConstructionSite.Areas.ConstructionAdmin.Controllers
            };
             if (customerFeedbackUpdate==null)
             {
+                _unitOfWork.Rollback();
                 ModelState.AddModelError("", "this data is null or empity");
             }
+            _unitOfWork.Dispose();
             return View(customerFeedbackUpdate);
         }
-        public IActionResult Update()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(CustomerFeedback customerFeedback)
         {
+            if (customerFeedback==null)
+            {
+                ModelState.AddModelError("", "this data is null or empity");
+            }
+            var customerFeedbackUpdateResult=await  _unitOfWork.customerFeedbackRepository.UpdateAsync(customerFeedback);
+            if (!customerFeedbackUpdateResult.IsDone)
+            {
+                ModelState.AddModelError("", "this data is null or empity");
+            }
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                return Json(new
+                {
+                    message = "BadRequest"
+                });
+            }
+            if (id<1)
+            {
+                ModelState.AddModelError("", "id is null");
+
+            }
+            var customerFeedbackResult= await _unitOfWork.customerFeedbackRepository.GetByIdAsync(id);
+            if (customerFeedbackResult!=null)
+            {
+                ModelState.AddModelError("", "this data is null or empity");
+            }
+            var customerFeedbackDeleteResult=await  _unitOfWork.customerFeedbackRepository.DeleteAsync(customerFeedbackResult);
+            if (!customerFeedbackDeleteResult.IsDone)
+            {
+                _unitOfWork.Rollback();
+                ModelState.AddModelError("", "this data is null or empity");
+            }
+            _unitOfWork.Dispose();
             return View();
         }
     }
