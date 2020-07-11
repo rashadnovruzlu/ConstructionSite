9888fe1f9d4e309acb4ebe33777f743abc631b67
Nurana Panahli <penahlinurana@gmail.com>
2020-07-11 12:41:18 +0300

    Blog uchun TagHelper yazdim

---
+++ ConstructionSite/TagHelpers/NewsTagHelpers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstructionSite.TagHelpers
+{
+    [HtmlTargetElement("p")]
+    public class NewsTagHelpers : TagHelper
+    {
+        [HtmlAttributeName("text")]
+        public string Text { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!string.IsNullOrEmpty(Text))
+            {
+                if (Text.Length < 150)
+                {
+                    output.Content.SetHtmlContent(Text.ToString());
+                }
+                else
+                {
+                    output.Content.SetHtmlContent(Text.Substring(0, 150).ToString());
+                }
+            }
+        }
+    }
+}
