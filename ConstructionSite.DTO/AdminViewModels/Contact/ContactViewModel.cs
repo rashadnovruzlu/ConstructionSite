8a90a211e0562d7dbd896f20aafde33bb8cb353d
Nurana Panahli <penahlinurana@gmail.com>
2020-07-11 18:46:02 +0300

    Contact uchun ViewModelleeri yaratdim

---
+++ ConstructionSite.DTO/AdminViewModels/Contact/ContactViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.DTO.AdminViewModels.Contact
+{
+    public class ContactViewModel
+    {
+        
+    }
+}
