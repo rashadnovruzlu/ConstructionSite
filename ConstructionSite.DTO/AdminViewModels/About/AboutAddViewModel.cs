aa09a26a4f99fc32276e99a3caa4cc0b3e7f8d08
Naib Residov <residovnaib@gmail.com>
2020-07-12 15:08:37 +0300

    hhhhhhhhhhhhhhhh

--- ConstructionSite.DTO/AdminViewModels/About/AboutAddViewModel.cs
+++ ConstructionSite.DTO/AdminViewModels/About/AboutAddViewModel.cs
@@ -8,15 +8,19 @@ namespace ConstructionSite.DTO.AdminViewModels.About
   public  class AboutAddViewModel
     {
         public int Id { get; set; }
+		[Display(Name = "TittleAz")]
 		[Required(ErrorMessage = "TittleAz is Required")]
         public string TittleAz { get; set; }
-
+		[Display(Name = "TittleEn")]
 		public string TittleEn { get; set; }
-
+		[Display(Name = "TittleRu")]
 		public string TittleRu { get; set; }
 		[Required(ErrorMessage = "ContentAz is Required")]
+		[Display(Name = "ContentAz")]
 		public string ContentAz { get; set; }
+		[Display(Name = "ContentEn")]
 		public string ContentEn { get; set; }
+	    [Display(Name = "ContentRu")]
 		public string ContentRu { get; set; }
 	}
 }
