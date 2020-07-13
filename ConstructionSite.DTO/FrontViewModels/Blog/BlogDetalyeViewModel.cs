93a0d4dcd701bda1a82f65f195f49b3292471a6c
Naib Residov <residovnaib@gmail.com>
2020-07-13 22:45:50 +0300

    Finished_Blog_Front

---
+++ ConstructionSite.DTO/FrontViewModels/Blog/BlogDetalyeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.DTO.FrontViewModels.Blog
+{
+   public class BlogDetalyeViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string imagePath { get; set; }
+        public DateTime dateTime { get; set; }
+    }
+}
--- ConstructionSite/Controllers/BlogController.cs
+++ ConstructionSite/Controllers/BlogController.cs
@@ -2,18 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ConstructionSite.DTO.AdminViewModels.News;
+using ConstructionSite.DTO.FrontViewModels.Blog;
+using ConstructionSite.Injections;
+using ConstructionSite.Localization;
+using ConstructionSite.Repository.Abstract;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Math.EC.Rfc7748;
 
 namespace ConstructionSite.Controllers
 {
     public class BlogController : Controller
     {
-        public IActionResult Index()
+
+        private string                        _lang;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUnitOfWork          _unitOfWork;
+      
+
+        public BlogController(IUnitOfWork unitOfWork,
+                              IHttpContextAccessor httpContextAccessor)
         {
-            return View();
+            _httpContextAccessor = httpContextAccessor;
+            _unitOfWork = unitOfWork;
+            _lang = _httpContextAccessor.getLang();
         }
 
-        public IActionResult Inner()
+        public async Task<IActionResult> Index(int id)
+        {
+            var result=await _unitOfWork.newsImageRepository.GetByIdAsync(id);
+          
+            
+            return View();
+        }
+        public async Task<IActionResult> Detalye(int id)
+        {
+            var newsImageResult=await _unitOfWork.newsImageRepository.GetByIdAsync(id);
+
+            var blogDetalyeViewModel = new BlogDetalyeViewModel
+            {
+                Id=newsImageResult.Id,
+                Title=newsImageResult.News.FindTitle(_lang),
+                Content=newsImageResult.News.FindContent(_lang),
+                dateTime=newsImageResult.News.CreateDate,
+               
+                imagePath=newsImageResult.Image.Path,
+                
+                
+            }
+;
+            return View(blogDetalyeViewModel);
+        }
+
+        public IActionResult Deaty()
         {
             return View();
         }
