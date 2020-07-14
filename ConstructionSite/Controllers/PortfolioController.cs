d4d46f87e610c752b33375cbefb48af948ac3a41
Naib Residov <residovnaib@gmail.com>
2020-07-14 06:31:57 +0300

    Finsihed

--- ConstructionSite/Controllers/PortfolioController.cs
+++ ConstructionSite/Controllers/PortfolioController.cs
@@ -1,8 +1,11 @@
-﻿using ConstructionSite.DTO.AdminViewModels.Portfolio;
+﻿
+using ConstructionSite.DTO.FrontViewModels.Portfoli;
+using ConstructionSite.DTO.FrontViewModels.Portfolio;
 using ConstructionSite.Injections;
 using ConstructionSite.Repository.Abstract;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
 using System.Linq;
 
 namespace ConstructionSite.Controllers
@@ -39,6 +42,22 @@ namespace ConstructionSite.Controllers
         public IActionResult Detail()
         {
             return View();
+        }
+        public IActionResult Project(int id)
+        {
+
+        var result=  _unitOfWork.projectImageRepository.GetAll()
+                .Where(x=>x.ProjectId==id)
+                .Include(x=>x.Project)
+                .Include(x=>x.Image)
+                .Select(x=>new PoftfiloProjectViewModel
+                {
+                    id=x.Project.Id,
+                    Name=x.Project.FindName(_lang),
+                    Imagepath=x.Image.Path
+                })
+                .ToList();
+            return View(result);
         }
     }
 }
