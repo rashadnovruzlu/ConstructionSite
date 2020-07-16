d3a7094303cf6cbdaafc61b120188cd6665f25c3
Naib Residov <residovnaib@gmail.com>
2020-07-17 00:57:36 +0300

    Delete_User

--- ConstructionSite/Areas/ConstructionAdmin/Controllers/AccountController.cs
+++ ConstructionSite/Areas/ConstructionAdmin/Controllers/AccountController.cs
@@ -19,10 +19,10 @@ namespace ConstructionSite.Areas.Admin.Controllers
     public class AccountController : Controller
     {
         #region Fields
-        private readonly UserManager<ApplicationUser> userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly ApplicationIdentityDbContext _identityDb;
+      //  private readonly ApplicationIdentityDbContext _identityDb;
         #endregion
 
         #region AddErrors Method
@@ -38,10 +38,10 @@ namespace ConstructionSite.Areas.Admin.Controllers
         #region CTOR
         public AccountController(ApplicationIdentityDbContext identityDb, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
-            this.userManager = userManager;
+            this._userManager = userManager;
             this._signInManager = signInManager;
             this._roleManager = roleManager;
-            this._identityDb = identityDb;
+          //  this._identityDb = identityDb;
         }
         #endregion
 
@@ -50,14 +50,8 @@ namespace ConstructionSite.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            IEnumerable<UserDTO> users = userManager.Users.Select(m => new UserDTO
-            {
-                Id = m.Id,
-                Name = m.Name,
-                Email = m.Email,
-                UserName = m.UserName
-            });
-            return View(users);
+          
+            return View();
         }
 
         #endregion
@@ -78,39 +72,28 @@ namespace ConstructionSite.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                try
+
+            }
+         
+            ApplicationUser user=new ApplicationUser();
+            user.UserName=viewModel.Username;
+            user.Email=viewModel.Email;
+          var result=  await  _userManager.CreateAsync(user,viewModel.Password);
+            if (result.Succeeded)
+            {
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                foreach (var item in result.Errors)
                 {
-                    ApplicationUser user = new ApplicationUser
-                    {
-                        UserName = viewModel.Username,
-                        Email = viewModel.Email,
-                        Name = viewModel.Name
-                    };
-
-                    IdentityResult result = await userManager.CreateAsync(user, viewModel.Password);
-
-                    if (result.Succeeded)
-                    {
-                        ApplicationUser appUser = await userManager.FindByEmailAsync(user.Email);
-
-                        return RedirectToAction("Index", "Account", new { Areas = "ConstructionAdmin" });
-                    }
-                    else
-                    {
-                        AddErrors(result);
-
-                        if (ModelState.ErrorCount != 0)
-                        {
-                            return View(viewModel);
-                        }
-                    }
-                }
-                catch
-                {
-                    ModelState.AddModelError("", "Some error occured. Please try again.");
+                    ModelState.AddModelError("",item.Description.ToString());
                 }
             }
-            return View(viewModel);
+
+            return View();
         }
 
         #endregion
@@ -139,7 +122,7 @@ namespace ConstructionSite.Areas.Admin.Controllers
             {
                 try
                 {
-                    ApplicationUser appUser = await userManager.FindByEmailAsync(loginModel.Email);
+                    ApplicationUser appUser = await _userManager.FindByEmailAsync(loginModel.Email);
 
                     if (appUser != null)
                     {
@@ -175,14 +158,14 @@ namespace ConstructionSite.Areas.Admin.Controllers
         [Route("Edit")]
         public async Task<IActionResult> Edit(string id)
         {
-            ApplicationUser appUser = await userManager.GetUserAsync(User);
+            ApplicationUser appUser = await _userManager.GetUserAsync(User);
 
             if (appUser == null)
             {
                 ModelState.AddModelError("", "User or Admin is empty");
             }
 
-            var userRole = await _identityDb.UserRoles.Where(m => m.UserId == appUser.Id).FirstOrDefaultAsync();
+            var userRole = await _userManager.UserRoles.Where(m => m.UserId == appUser.Id).FirstOrDefaultAsync();
             var roles = await _roleManager.Roles.ToListAsync();
             return View(new UserEditModel
             {
@@ -202,15 +185,15 @@ namespace ConstructionSite.Areas.Admin.Controllers
             {
                 try
                 {
-                    ApplicationUser appUser = await userManager.FindByIdAsync(id);
+                    ApplicationUser appUser = await _userManager.FindByIdAsync(id);
 
                     if (appUser == null)
                         throw new NullReferenceException();
                     appUser.Email = userEditModel.Email;
                     appUser.Name = userEditModel.Name;
                     appUser.UserName = userEditModel.Username;
-                    appUser.PasswordHash = (!String.IsNullOrWhiteSpace(userEditModel.Password)) ? userManager.PasswordHasher.HashPassword(appUser, userEditModel.Password) : appUser.PasswordHash;
-                    IdentityResult result = await userManager.UpdateAsync(appUser);
+                    appUser.PasswordHash = (!String.IsNullOrWhiteSpace(userEditModel.Password)) ? _userManager.PasswordHasher.HashPassword(appUser, userEditModel.Password) : appUser.PasswordHash;
+                    IdentityResult result = await _userManager.UpdateAsync(appUser);
 
                     if (result.Succeeded)
                     {
@@ -258,46 +241,27 @@ namespace ConstructionSite.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(string id)
         {
-            if (ModelState.IsValid)
+        
+            var userResult=await _userManager.FindByIdAsync(id);
+            if (userResult==null)
             {
-                IDbContextTransaction transaction = null;
-                try
+          var identityResult=   await   _userManager.DeleteAsync(userResult);
+                if (!identityResult.Succeeded)
                 {
-                    var currentUser = await userManager.GetUserAsync(HttpContext.User);
-
-                    if (currentUser.Id == id)
-                        throw new Exception("You can not delete own");
-
-                    ApplicationUser user = await userManager.FindByIdAsync(id);
-
-                    if (user == null)
-                        throw new NullReferenceException();
-
-                    IdentityResult result = await userManager.DeleteAsync(user);
-
-                    if (!result.Succeeded)
-                        throw new Exception();
-                    transaction.Commit();
-                    return Json(new
+                    foreach (var item in identityResult.Errors)
                     {
-                        status = HttpStatusCode.OK
-                    });
+                        ModelState.AddModelError("",item.Description.ToString());
+                    }
                 }
-                catch (Exception)
+                else
                 {
-                    transaction.Rollback();
-                }
-                finally
-                {
-                    if (transaction != null)
-                        transaction.Dispose();
+
+                    ModelState.AddModelError("","this user not exists curret data");
                 }
             }
+           
+            return View("Index",_userManager.Users);
 
-            return Json(new
-            {
-                status = HttpStatusCode.NotFound
-            });
         }
 
         #endregion
