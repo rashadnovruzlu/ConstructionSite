818cbf04f981125a780a89bb64dded136154250b
Naib Residov <residovnaib@gmail.com>
2020-07-28 18:48:28 +0300

    After_Merge

--- ConstructionSite/Controllers/ServicesController.cs
+++ ConstructionSite/Controllers/ServicesController.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 
 namespace ConstructionSite.Controllers
 {
