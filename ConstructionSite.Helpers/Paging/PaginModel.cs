f47d7e7b5f8e4765a968b8bc5366accae23b3729
Naib Residov <residovnaib@gmail.com>
2020-07-29 23:59:43 +0300

    Deleted_Pagination_TagHeloer

---
+++ ConstructionSite.Helpers/Paging/PaginModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ConstructionSite.Helpers.Paging
+{
+    public class PaginModel<T>
+    {
+        public IEnumerable<T>  Paging { get; set; }
+        public PagingInfo  PagingInfo { get; set; }
+    }
+}
