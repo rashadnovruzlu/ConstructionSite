0de7d88e02b008a3683f1e306e72f4f54a348cda
Naib Residov <residovnaib@gmail.com>
2020-08-10 12:37:07 +0300

    Added_Javascrip_File

---
+++ ConstructionSite/wwwroot/customer/js/_initMap.js
@@ -0,0 +1,11 @@
+﻿ let map;
+
+      function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+          center: {
+                lat: 40.409264,
+                lng: 49.867092
+          },
+          zoom: 5
+        });
+      }
---
+++ ConstructionSite/wwwroot/customer/js/_mapMarker.js
@@ -0,0 +1,15 @@
+﻿function initMap() {
+    const myLatLng = {
+        lat: 40.409264,
+        lng: 49.867092
+    };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: myLatLng
+    });
+    new google.maps.Marker({
+        position: myLatLng,
+        map,
+        title: "Hello World!"
+    });
+}
