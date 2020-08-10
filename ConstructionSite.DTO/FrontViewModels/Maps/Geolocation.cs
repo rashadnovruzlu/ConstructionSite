e4c1352cdfa4923712fdfcca9498ca834743e533
Naib Residov <residovnaib@gmail.com>
2020-08-10 15:00:01 +0300

    Finsihed_Maps

---
+++ ConstructionSite.DTO/FrontViewModels/Maps/Geolocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.DTO.FrontViewModels.Maps
+{
+   public class Geolocation
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
--- ConstructionSite/Controllers/MessageController.cs
+++ ConstructionSite/Controllers/MessageController.cs
@@ -1,10 +1,12 @@
-﻿using ConstructionSite.DTO.FrontViewModels.Message;
+﻿using ConstructionSite.DTO.FrontViewModels.Maps;
+using ConstructionSite.DTO.FrontViewModels.Message;
 using ConstructionSite.Entity.Models;
 using ConstructionSite.Helpers.Constants;
 using ConstructionSite.Localization;
 using ConstructionSite.Repository.Abstract;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
 using System.Net;
 
 namespace ConstructionSite.Controllers
@@ -26,6 +28,16 @@ namespace ConstructionSite.Controllers
 
         public IActionResult Index()
         {
+         var _googelMapGeolocation=  _unitOfWork
+                .ContactRepository
+                .GetAll()
+                .Select(x=>new Geolocation
+                {
+                    Latitude=x.lat,
+                    Longitude=x.lng
+                }).FirstOrDefault();
+            ViewBag.la=_googelMapGeolocation.Latitude;
+            ViewBag.lo=_googelMapGeolocation.Longitude;
             return View();
         }
 
