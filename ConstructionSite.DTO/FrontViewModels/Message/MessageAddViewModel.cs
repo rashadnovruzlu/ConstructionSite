885c380d5a185ea9e3266e9921ff15553af18019
Naib Residov <residovnaib@gmail.com>
2020-08-13 19:53:38 +0300

    _yesy

--- ConstructionSite.DTO/FrontViewModels/Message/MessageAddViewModel.cs
+++ ConstructionSite.DTO/FrontViewModels/Message/MessageAddViewModel.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ConstructionSite.DTO.FrontViewModels.Message
 {
     public class MessageAddViewModel
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
-
+        [Required]
         public string Subject { get; set; }
-
+        [Required]
         public string UserMessage { get; set; }
         public DateTime SendDate { get; set; }
         public bool IsAnswerd { get; set; }
