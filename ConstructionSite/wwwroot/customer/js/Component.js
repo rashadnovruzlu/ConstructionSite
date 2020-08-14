fd4f65f468b1500a713ea3e1619b7ac9cba96a2a
Naib Residov <residovnaib@gmail.com>
2020-08-14 03:55:11 +0300

    Finished

--- ConstructionSite/wwwroot/customer/js/Component.js
+++ ConstructionSite/wwwroot/customer/js/Component.js
@@ -1,13 +1,56 @@
 ﻿var id = 0;
-$(document).ready(function () {
+
+        $(document).ready(function () {
+
+        $("#grid-container").load(`/Portfolio/Project/${id}`);
+            $("#myfist").removeClass("cbp-filter-item  ");
+            $("#myfist").addClass("cbp-filter-item   cbp-filter-item-active");
+            $(".All").click(function () {
+        $("#myfist").removeClass("cbp-filter-item");
+                $("#myfist").addClass("cbp-filter-item cbp-filter-item-active");
+                $("#grid-container").load(`/Portfolio/Project/${id}`);
+            });
+            $(".mains").click(function () {
+        id = $(this).data("id");
+
+                $("#myfist").removeClass("cbp-filter-item cbp-filter-item-active");
+                $("#myfist").addClass("cbp-filter-item");
+
+                $("#grid-container").load(`/Portfolio/Project/${id}`);
+            });
+
+        });
+//$(document).ready(function () {
    
   
-    $("#grid-container").load(`/Portfolio/Project/${id=0}`);
-    $(".All").click(function () {
-        $("#grid-container").load(`/Portfolio/Project/${id=0}`);
-    });
-    $(".mains").click(function () {
-        id = $(this).data("id");
-        $("#grid-container").load(`/Portfolio/Project/${id}`);
-    });
-});
+//    $("#grid-container").load(`/Portfolio/Project/${id=0}`);
+//    $(".All").click(function () {
+//        $("#grid-container").load(`/Portfolio/Project/${id=0}`);
+//    });
+//    $(".mains").click(function () {
+//        id = $(this).data("id");
+//        $("#grid-container").load(`/Portfolio/Project/${id}`);
+//    });
+//});
+
+
+        //$(document).ready(function () {
+        //    var id = 0;
+        //    $("#grid-container").load(`/Portfolio/Project/${id}`);
+        //    $("#myfist").removeClass("cbp-filter-item");
+        //    $("#myfist").addClass("cbp-filter-item cbp-filter-item-active");
+        //    $(".All").click(function () {
+        //    $("#myfist").removeClass("cbp-filter-item");
+        //        $("#myfist").addClass("cbp-filter-item cbp-filter-item-active");
+        //        $("#grid-container").load(`/Portfolio/Project/${id}`);
+        //    });
+        //    $(".mains").click(function () {
+        //    id = $(this).data("id");
+
+        //        $("#myfist").removeClass("cbp-filter-item cbp-filter-item-active");
+        //        $("#myfist").addClass("cbp-filter-item");
+
+        //        $("#grid-container").load(`/Portfolio/Project/${id}`);
+        //    });
+
+        //});
