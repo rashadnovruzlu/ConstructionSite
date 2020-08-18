b28679d9333246f5d82aae5b59d3f6bd8357b543
Naib Residov <residovnaib@gmail.com>
2020-08-18 16:29:56 +0300

    RequestCurrentLang

--- ConstructionSite/Injections/RequestCurrentLang.cs
+++ ConstructionSite/Injections/RequestCurrentLang.cs
@@ -1,17 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ConstructionSite.Injections
 {
-   public static class RequestCurrentLang
+    public static class RequestCurrentLang
     {
         public static string GetLanguages(this IHttpContextAccessor _httpContextAccessor)
         {
-           var data = _httpContextAccessor.HttpContext.Request;
+            var data = _httpContextAccessor.HttpContext.Request;
             var requestCultureFeature = data.HttpContext.Features.Get<IRequestCultureFeature>();
             var requestCulture = requestCultureFeature.RequestCulture.UICulture;
             return requestCulture.Name;
