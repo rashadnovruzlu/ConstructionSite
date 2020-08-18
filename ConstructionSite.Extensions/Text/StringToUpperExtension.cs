14182d9ea9568b243cbafd97e8bcd6c22dc5ec0c
Naib Residov <residovnaib@gmail.com>
2020-08-18 13:26:06 +0300

    languagesResult.ToUpper

---
+++ ConstructionSite.Extensions/Text/StringToUpperExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.Extensions.Text
+{
+  public static   class StringToUpperExtension
+    {
+        public static string FistUpper(this string str)
+        {
+            StringBuilder builder=new StringBuilder();
+            string strFirstLetter = str
+                   .Substring(0, 1)
+                   .ToUpper();
+          var strFulllanguages=  builder.Append(strFirstLetter + str.Substring(1)).ToString();
+           // string strFulllanguages = strFirstLetter + str.Substring(1);
+            return strFulllanguages;
+        }
+    }
+}
