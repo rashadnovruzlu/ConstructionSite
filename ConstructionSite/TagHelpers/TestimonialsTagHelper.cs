a544e782978e2dab4ab659c8f50dfc92f4484264
Naib Residov <residovnaib@gmail.com>
2020-09-01 18:30:00 +0300

    testimonialtext

--- ConstructionSite/TagHelpers/TestimonialsTagHelper.cs
+++ ConstructionSite/TagHelpers/TestimonialsTagHelper.cs
@@ -2,23 +2,23 @@
 
 namespace ConstructionSite.TagHelpers
 {
-    [HtmlTargetElement("blockquote")]
+    [HtmlTargetElement("blockquote",Attributes = "testimonial-text")]
     public class TestimonialsTagHelper : TagHelper
     {
-        [HtmlAttributeName("testimonialtext")]
-        public string Text { get; set; }
+       
+        public string TestimonialText { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            if (!string.IsNullOrEmpty(Text))
+            if (!string.IsNullOrEmpty(TestimonialText))
             {
-                if (Text.Length < 300)
+                if (TestimonialText.Length < 300)
                 {
-                    output.Content.SetHtmlContent(Text.ToString());
+                    output.Content.SetHtmlContent(TestimonialText.ToString());
                 }
                 else
                 {
-                    output.Content.SetHtmlContent(Text.Substring(0, 300).ToString());
+                    output.Content.SetHtmlContent(TestimonialText.Substring(0, 300).ToString());
                 }
             }
         }
