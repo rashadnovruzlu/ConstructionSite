545c22ea5877f9972e376cad0d444b645bb06abc
Naib Residov <residovnaib@gmail.com>
2020-10-16 16:20:23 +0300

    builder_For_EFCore

---
+++ ConstructionSite.Extensions/TypeBuilder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ConstructionSite.Extensions
+{
+    public static class TypeBuilder
+    {
+        public static void builder<T>(EntityTypeBuilder<T> typeBuilder) where T : class
+        {
+            Type myType = typeBuilder.GetType();
+            foreach (var item in myType.GetProperties())
+            {
+                if (item.PropertyType == typeof(bool))
+                {
+                    item.SetValue(item, false);
+                }
+            }
+        }
+    }
+}
