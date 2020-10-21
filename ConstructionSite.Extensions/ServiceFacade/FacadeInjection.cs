07784b138b46a745e24d1516f6d6bcd5ce3dcb8e
Naib Residov <residovnaib@gmail.com>
2020-10-21 13:57:11 +0300

    Added_FacadeService_Inajection_Extension

---
+++ ConstructionSite.Extensions/ServiceFacade/FacadeInjection.cs
@@ -0,0 +1,16 @@
+﻿using ConstructionSite.Interfaces.Facade;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.Extensions.ServiceFacade
+{
+   public static class FacadeInjection
+    {
+        public static void LoadFacade(this IServiceCollection services)
+        {
+            services.AddTransient<IGaleryFacade, GaleryFacade>();
+        }
+    }
+}
