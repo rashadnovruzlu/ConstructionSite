16a2e432dd4028db0458adbd8d6931a86b287ae3
Naib Residov <residovnaib@gmail.com>
2020-10-21 12:17:50 +0300

    Finsihed_AllResptory

---
+++ ConstructionSite.Repository/Implementations/ServiceImageRepstory.cs
@@ -0,0 +1,18 @@
+﻿using ConstructionSite.DTO.FrontViewModels.Service;
+using ConstructionSite.Entity.Data;
+using ConstructionSite.Repository.Concreate;
+using ConstructionSite.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.Repository.Implementations
+{
+   public class ServiceImageRepstory: GenericRepository<ServiceImage>, IServiceImageRepstory
+    {
+        public ServiceImageRepstory(ConstructionDbContext context):base(context)
+        {
+
+        }
+    }
+}
---
+++ ConstructionSite.Repository/Interfaces/IServiceImageRepstory.cs
@@ -0,0 +1,12 @@
+﻿using ConstructionSite.DTO.FrontViewModels.Service;
+using ConstructionSite.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.Repository.Interfaces
+{
+    public interface IServiceImageRepstory : IGenericRepository<ServiceImage>
+    {
+    }
+}
