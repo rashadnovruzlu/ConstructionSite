1884eb39265b38b3e9ced7d75bb7837523c1ec45
Naib Residov <residovnaib@gmail.com>
2020-10-27 21:10:31 +0200

    Finsihed

--- ConstructionSite/Areas/ConstructionAdmin/Controllers/ImageController.cs
+++ ConstructionSite/Areas/ConstructionAdmin/Controllers/ImageController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ConstructionSite.Interface.Facade.Images;
+using ConstructionSite.Repository.Abstract;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,8 +11,24 @@ namespace ConstructionSite.Areas.ConstructionAdmin.Controllers
 {
     public class ImageController : Controller
     {
-        public IActionResult Update(object[] id)
+        private readonly IImageFacade _imageFacade;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ImageController(IImageFacade imageFacade,
+                               IUnitOfWork unitOfWork)
         {
+            _imageFacade = imageFacade;
+            _unitOfWork = unitOfWork;
+        }
+
+
+        public async Task<IActionResult> blogUpdate(IFormFile file, int id)
+        {
+            var result = await _unitOfWork.imageRepository.FindAsync(x => x.Id == id);
+            if (result != null)
+            {
+               
+            }
             return View();
         }
 
