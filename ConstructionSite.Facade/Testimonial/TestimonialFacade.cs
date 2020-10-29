bf1ac7759ea2d202fe13d88cb6322bb25750a411
Naib Residov <residovnaib@gmail.com>
2020-10-29 10:39:53 +0200

    TestimonialFacade

---
+++ ConstructionSite.Facade/Testimonial/TestimonialFacade.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.Facade.Testimonial
+{
+   public class TestimonialFacade
+    {
+    }
+}
---
+++ ConstructionSite.Interface/Facade/Testimonial/ITestimonialFacade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.Interface.Facade.Testimonial
+{
+   public interface ITestimonialFacade
+    {
+        
+    }
+}
