a5d91651c49e2e98c1105a18a175131aea9bad2d
Naib Residov <residovnaib@gmail.com>
2020-10-30 11:54:18 +0200

    Create_GaleryViewoModel

---
+++ ConstructionSite.DTO/FrontViewModels/Galery/GaleryImageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.ViwModel.FrontViewModels.Galery
+{
+    public class GaleryImageViewModel
+    {
+        public List<string> Imagepaths { get; set; }
+    }
+
+}
---
+++ ConstructionSite.DTO/FrontViewModels/Galery/GaleryViewoModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionSite.ViwModel.FrontViewModels.Galery
+{
+    public class GaleryViewoModel
+    {
+        public List<string> Vidopaths { get; set; }
+    }
+}
