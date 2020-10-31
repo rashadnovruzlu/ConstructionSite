d417d8c1481b9d76d726b994f5a8b91596afcbe9
Naib Residov <residovnaib@gmail.com>
2020-10-31 13:00:39 +0200

    Finsihed

--- ConstructionSite/TagHelpers/BlogTitileTagHelpers.cs
+++ ConstructionSite/TagHelpers/BlogTitileTagHelpers.cs
@@ -3,7 +3,7 @@
 namespace ConstructionSite.TagHelpers
 {
     [HtmlTargetElement("h1", Attributes = "blog-title")]
-    public class BlogTitleTagHelpers : TagHelper
+    public class BlogTdTitleTagHelpers : TagHelper
     {
         public string BlogTitle { get; set; }
 
@@ -22,4 +22,24 @@ namespace ConstructionSite.TagHelpers
             }
         }
     }
+    //[HtmlTargetElement("td", Attributes = "blog-title")]
+    //public class BlogTdTitleTagHelpers : TagHelper
+    //{
+    //    public string BlogTitle { get; set; }
+
+    //    public override void Process(TagHelperContext context, TagHelperOutput output)
+    //    {
+    //        if (!string.IsNullOrEmpty(BlogTitle))
+    //        {
+    //            if (BlogTitle.Length < 27)
+    //            {
+    //                output.Content.SetHtmlContent(BlogTitle.ToString());
+    //            }
+    //            else
+    //            {
+    //                output.Content.SetHtmlContent(BlogTitle.Substring(0, 26).ToString());
+    //            }
+    //        }
+    //    }
+    //}
 }
