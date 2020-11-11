a69019bf9b1c51c9c5143c4d7c4ff589164d5ee6
Naib Residov <residovnaib@gmail.com>
2020-11-11 23:50:49 +0200

    AddedSlider_resptory

--- ConstructionSite.Entity/Models/Sliders.cs
+++ ConstructionSite.Entity/Models/Sliders.cs
@@ -8,6 +8,7 @@ namespace ConstructionSite.Entity.Models
     public class Sliders : TitleContent
     {
         public int Id { get; set; }
+        public string ImagePath { get; set; }
 
     }
 }
